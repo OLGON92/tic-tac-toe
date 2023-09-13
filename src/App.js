2cabd1082f7bb76222bfd50188b46279f3d72de8
OLGON92 <ogon318@gmail.com>
2023-09-13 15:48:23 -0700

    add value prop from Board to Square element, and fix syntax error on line 12

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
+import React from "react";
+
 //Reads value prop that will pass from Board
 function Square({ value }) {
-  return <button className="square">1</button>;
+  //Curly braces around value renders the variable value
+  return <button className="square">{value}</button>;
 }
 
 export default function Board() {
+  //Adding value prop to each sqaure makes it so that Board comp passes value prop to each Square comp it renders
   return (
-    <>
+    <React.Fragment>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value="1" />
+        <Square value="2" />
+        <Square value="3" />
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value="4" />
+        <Square value="5" />
+        <Square value="6" />
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value="7" />
+        <Square value="8" />
+        <Square value="9" />
       </div>
-    </>
+    </React.Fragment>
   );
 }
