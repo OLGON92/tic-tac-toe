18d6af28c647572ece7807f27b986c4b79df5d49
OLGON92 <ogon318@gmail.com>
2023-09-13 17:48:10 -0700

    re-factor Board to include a state variable, and also edit out stateful tracking for Square and the button's onClick prop

--- src/App.js
+++ src/App.js
@@ -1,40 +1,31 @@
 import React, { useState } from "react";
 
 //Reads value prop that will pass from Board
-function Square() {
-  const [value, setValue] = useState(null);
-  //This function will be called when a square is clicked
-  function handleClick() {
-    setValue('X');
-  }
+function Square({value}) {
   //Curly braces around value renders the variable value
-  return (
-    <button 
-      className="square" 
-      onClick={handleClick}
-    >
-      {value}
-    </button>
-  );
+  return 
+    <button className="square">{value}</button>;
 }
 
 export default function Board() {
+  //Array(9).fill(null) creates an array of 9 elements set to null corresponding to 9 squares
+  const [squares, setSquares] = useState(Array(9).fill(null));
   return (
     <React.Fragment>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[0]} />
+        <Square value={squares[1]} />
+        <Square value={squares[2]} />
       </div>
       <div className="board-row">
-        <Square  />
-        <Square />
-        <Square />
+        <Square value={squares[3]} />
+        <Square value={squares[4]} />
+        <Square value={squares[5]} />
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[6]} />
+        <Square value={squares[7]} />
+        <Square value={squares[8]} />
       </div>
     </React.Fragment>
   );
