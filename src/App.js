57e1d23a20fc94101e6709e326f34113b3ba0171
OLGON92 <ogon318@gmail.com>
2023-09-13 17:35:01 -0700

    add a state variable to square

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 
 //Reads value prop that will pass from Board
-function Square({ value }) {
+function Square() {
+  const [value, setValue] = useState(null);
+  //This function will be called when a square is clicked
   function handleClick() {
-    console.log("clicked");
+    setValue('X');
   }
   //Curly braces around value renders the variable value
   return (
@@ -17,23 +19,22 @@ function Square({ value }) {
 }
 
 export default function Board() {
-  //Adding value prop to each sqaure makes it so that Board comp passes value prop to each Square comp it renders
   return (
     <React.Fragment>
       <div className="board-row">
-        <Square value="1" />
-        <Square value="2" />
-        <Square value="3" />
+        <Square />
+        <Square />
+        <Square />
       </div>
       <div className="board-row">
-        <Square value="4" />
-        <Square value="5" />
-        <Square value="6" />
+        <Square  />
+        <Square />
+        <Square />
       </div>
       <div className="board-row">
-        <Square value="7" />
-        <Square value="8" />
-        <Square value="9" />
+        <Square />
+        <Square />
+        <Square />
       </div>
     </React.Fragment>
   );
